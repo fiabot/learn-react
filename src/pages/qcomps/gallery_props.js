90d1ec93b61dc34c5e20b18e53a41f51557336df
Fiona Shyne <shynef@union.edu>
2024-02-07 16:26:00 -0500

    exercieses

--- src/pages/qcomps/gallery_props.js
+++ src/pages/qcomps/gallery_props.js
@@ -1,55 +1,53 @@
+function Bio({name, imgSrc, profession, awards, discovered}){
+  return(
+  <section className="profile">
+  <h2>{name}</h2>
+  <img
+    className="avatar"
+    src={imgSrc}
+    alt={name}
+    width={70}
+    height={70}
+  />
+  <ul>
+    <li>
+      <b>Profession: </b>
+      {profession}
+    </li>
+    <li>
+      <b>Awards: 4 </b>
+      {awards}
+    </li>
+    <li>
+      <b>Discovered: </b>
+      {discovered}
+    </li>
+  </ul>
+</section>);
+}
+
 export default function Gallery() {
   return (
     <div>
+  
       <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/szV5sdGs.jpg'
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/YfeOqp2s.jpg'
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
+
+      <Bio name ="Maria Skłodowska-Curie" 
+          imgSrc='https://i.imgur.com/szV5sdGs.jpg'
+          profession="physicist and chemist"
+          awards=  "(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)" 
+          discovered= "polonium (element)"
+           />
+
+      <Bio 
+        name = "Katsuko Saruhashi"
+            imgSrc='https://i.imgur.com/YfeOqp2s.jpg' 
+            profession="geochemist" 
+            awards="(Miyake Prize for geochemistry, Tanaka Prize)"
+            discovered="a method for measuring carbon dioxide in seawater"/>
+
+      
+      
     </div>
   );
 }
