f748108592a7cff1f826c09500b8e22e47362e68
Fiona Shyne <shynef@union.edu>
2024-02-12 15:13:09 -0500

    list keys

--- src/pages/qcomps/list_keys_id.js
+++ src/pages/qcomps/list_keys_id.js
@@ -17,7 +17,11 @@ function getImageUrl(imageId) {
 }
 export default function List() {
   const listItems = people.map(person =>
-    <li>
+    <li key = {person.imageId}>
+      <img
+      src={getImageUrl(person.imageId)}
+      alt= {person.name}
+    />
     </li>
   );
   return <ul>{listItems}</ul>;
